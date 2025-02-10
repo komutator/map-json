0024c943c38d3307961e5017e908fad82380574c
zeml <kommutator@yandex.ru>
2025-02-10 09:40:07 +0300

    Layout 3

---
+++ src/main/java/com/example/LabelPanel.java
@@ -0,0 +1,14 @@
+package com.example;
+import java.awt.*;
+import javax.swing.*;
+
+public class LabelPanel extends JPanel {
+    public LabelPanel() {
+//        setLayout(new GridLayout(3, 2)); // Две строки, два столбца
+
+        add(new JLabel("Карточка вызова"));
+
+    }
+}
+
+
