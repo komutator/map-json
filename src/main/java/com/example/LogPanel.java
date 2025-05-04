807d260c345e5710f43742fd5d9a517e8c064b5a
kzemlyanitsin_avangard-soft.ru <kzemlyanitsin@avangard-soft.ru>
2025-05-04 16:45:46 +0300

    log pane

--- src/main/java/com/example/LogPanel.java
+++ src/main/java/com/example/LogPanel.java
@@ -4,7 +4,21 @@ import javax.swing.*;
 import java.awt.*;
 
 public class LogPanel extends JPanel{
+
+    private JTextArea logText;
+
     public LogPanel(){
         add(new Label("Log here"));
+        logText = new JTextArea(3,100);
+
+        JScrollPane scrollPane = new JScrollPane(logText);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        add(scrollPane);
     }
+
+    public void addLog(String s){
+        logText.append(s);
+    }
+
 }
--- src/main/java/com/example/SouthPanel.java
+++ src/main/java/com/example/SouthPanel.java
@@ -4,7 +4,13 @@ public class SouthPanel extends AbstractPanel{
 
     public SouthPanel(){
         setBorderName("Log");
-        add(new LogPanel());
+        LogPanel log = new LogPanel();
+        log.addLog("Check DB\n");
+        log.addLog("Check DB1\n");
+        log.addLog("Check DB2\n");
+        log.addLog("Check DB3\n");
+        log.addLog("Check DB4\n");
+        add(log);
     }
 
 }
