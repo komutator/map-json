e19e6ba1fe3bab7f65e0938a739e8d7accfbee9d
kzeml_nout <kzemlyanitsin@avangard-soft.ru>
2025-06-10 09:36:42 +0300

    Tests addded

---
+++ src/main/test/com/example/MsdConvertHelperTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MsdConvertHelperTest {
+
+    @Test
+    void toVoiceMessagePresent() {
+    }
+
+    @Test
+    void toCoordinates() {
+    }
+}
